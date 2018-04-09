2f1856a095a82b814e8af99887e9dc40c5ae18fb
Alexey Shiklomanov <ashiklom@bu.edu>
2018-04-09 17:43:56 -0400

    New correlation analysis. Add some text.

--- sync_figures.sh
+++ sync_figures.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 rsync -avz ~/Projects/prospect-traits/rspecan/manuscript/figures/ figures
-
-for file in figures/prospect_pairs_*.pdf; do
-    convert -flatten $file ${file/pdf/png}
-done
