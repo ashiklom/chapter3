62cecab48ecbd2862e8855974d390759eeaf670d
Alexey Shiklomanov <ashiklom@bu.edu>
2018-04-09 00:28:11 -0400

    Update figs. Replace big pdfs with pngs

--- sync_figures.sh
+++ sync_figures.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 rsync -avz ~/Projects/prospect-traits/rspecan/manuscript/figures/ figures
+
+for file in figures/prospect_pairs_*.pdf; do
+    convert -flatten $file ${file/pdf/png}
+done
